c0d303b1a60d407aeb00ca6e4c5a6c4345f0797e
Philes Liew <philesliew@gmail.com>
2017-11-10 15:16:02 +0800

    Update new design

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
 (function () {
+  var typewriter = new Typewriter(document.getElementById('typingarea'), {
+      loop: true,
+      animateCursor: false
+  });
+  typewriter.typeString('Web Application')
+            .pauseFor(2500).deleteChars(12).typeString('site')
+            .pauseFor(2500).deleteChars(4).typeString(' Portal')
+            .pauseFor(2500).deleteAll().typeString('Restful API')
+            .pauseFor(2500).deleteAll().typeString('Microservice')
+            .pauseFor(2500).deleteAll().typeString('Mobile Application')
+            .pauseFor(2500).deleteAll().typeString('Microservice');
+  typewriter.start();
+
   if(! /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ){
     var bv = new Bideo();
     bv.init({
       // Video element
       videoEl: document.querySelector('#background_video'),
-
       // Container element
       container: document.querySelector('body'),
-
       // Resize
       resize: true,
-
       // Array of objects containing the src and type
       // of different video formats to add
       src: [
@@ -19,7 +29,6 @@
           type: 'video/mp4'
         }
       ],
-
       // What to do once video loads (initial frame)
       onLoad: function () {
         document.querySelector('#video_cover').style.display = 'none';
